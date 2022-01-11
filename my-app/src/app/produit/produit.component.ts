890ef48c692cd0227308f7d09faf9b4a5db71c5a
didierDefrance <didier@d-defrance.fr>
2022-01-11 17:12:38 +0100

    s

--- my-app/src/app/produit/produit.component.ts
+++ my-app/src/app/produit/produit.component.ts
@@ -9,7 +9,24 @@ import { Produit } from '../common/data/produit';
 export class ProduitComponent implements OnInit {
 
   produit : Produit = new Produit(); //produit sélectionné (à modifier ou à supprimer ou ....)
-  //...
+  
+  onSelectionnerProduit(p:Produit){
+     this.produit = p; //variante simpliste (idéalement clonage)
+  }
+
+  onSupprimerSelection(){
+    for(let i in this.produits){
+        if(this.produit.ref == this.produits[i].ref){
+          delete this.produits[i]; break;
+        }
+    }
+ }
+
+ onAjouterNew(){
+   this.produit = new Produit();
+   this.produits.push(this.produit);
+ }
+  
   produits : Produit[] = [
     new Produit('p1' , "Cahier" , 2.34) ,
     new Produit('p2' , 'Classeur' , 3.4)
