444eeb01b417e7894e3609ee8b861580aa1b527d
didierDefrance <didier@d-defrance.fr>
2022-01-11 16:09:35 +0100

    avec menu deroulant

--- my-app/src/app/header/header.component.ts
+++ my-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MenuDefinition } from 'src/bs-util/data/MenuDefinition';
 import { PreferencesService } from '../common/service/preferences.service';
 
 @Component({
@@ -11,16 +12,28 @@ export class HeaderComponent implements OnInit {
   @Input()
   public titre : string ="titre_par_defaut";
 
+
+  myMenuDef : MenuDefinition[] = [
+    { label : "Autres" , 
+      children : [
+        { label : "welcome" , path : "/ngr-welcome" } ,
+        { divider : true },
+        { label : "conversion" , path : "/ngr-conversion" }
+      ]
+    },
+    { label : "basic" , path : "/ngr-basic" } , 
+    { label : "login" , path : "/ngr-login" }
+    ];
+  
+
   //injection de dépendance par constructeur
   constructor(public preferencesService : PreferencesService){
-
+      console.log("dans constructeur : titre="+this.titre);
   }
 
   //equivalent de @PostConstruct de java
   ngOnInit(): void {
-  
+    console.log("dans ngOnInit : titre="+this.titre);
   }
   
-
-
 }
