7103ac28555dae953ac7a9dda7ffb76f78af023a
didierDefrance <didier@d-defrance.fr>
2022-01-13 14:48:20 +0100

    s

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   { path: 'ngr-basic', component: BasicComponent,
   children: [
     { path: 'tva', component: TvaComponent },
-    { path: 'calculatrice', component: CalculatriceComponent },
+    { path: 'calculatrice/:mode', component: CalculatriceComponent },
     { path: '', redirectTo: 'tva', pathMatch: 'prefix'}
     ] 
   },
--- my-app/src/app/basic/calculatrice/calculatrice.component.ts
+++ my-app/src/app/basic/calculatrice/calculatrice.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+
 @Component({
   selector: 'app-calculatrice',
   templateUrl: './calculatrice.component.html',
